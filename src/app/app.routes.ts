fbbe849d999b4d5f1ea24a24b28b308bfabeb9d2
diegoandres-dt <diegodelatorre155@gmail.com>
2025-07-10 13:18:45 -0500

    configuracion inicial de tab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'principal',
+    loadComponent: () => import('./pages/principal/principal.page').then( m => m.PrincipalPage)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/search/search.page').then( m => m.SearchPage)
+  },
+  {
+    path: 'library',
+    loadComponent: () => import('./pages/library/library.page').then( m => m.LibraryPage)
   },
 ];
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {IonicModule} from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { library, playCircle, radio, search } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-home',
@@ -9,5 +12,7 @@ import { CommonModule } from '@angular/common';
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+     addIcons({ library, playCircle, radio, search });
+  }
 }
