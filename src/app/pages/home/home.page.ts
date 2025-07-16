58d429da8ba1bba4f2a0d77f9db45289e4d4d387
diegoandres-dt <diegodelatorre155@gmail.com>
2025-07-16 18:47:03 -0500

    actualizacion cambios de color

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,18 +2,33 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { addIcons } from 'ionicons';
-import { library, playCircle,search } from 'ionicons/icons';
+import { library, playCircle, search } from 'ionicons/icons';
 import { MiSliderComponent } from 'src/app/components/mi-slider/mi-slider.component';
-
+import { Router } from '@angular/router';
+import { ellipsisVertical } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonicModule,CommonModule,MiSliderComponent],
+  imports: [IonicModule, CommonModule, MiSliderComponent],
 })
 export class HomePage {
-  constructor() {
-     addIcons({ library, playCircle,search });
+  tipoModo = 'Cambiar a modo Oscuro';
+  isDarkMode = false;
+
+  constructor(private router: Router) {
+    addIcons({ library, playCircle, search,ellipsisVertical });
   }
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    this.tipoModo = this.isDarkMode ? 'Cambiar a modo Lithg' : 'Cambiar a modo Oscuro';
+
+    document.body.classList.toggle('dark', this.isDarkMode);
+  }
+
+  sendIntro() {
+    this.router.navigate(['/'])
+  }
+
 }
