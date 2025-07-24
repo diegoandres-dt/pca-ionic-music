1bcf2d855837e81d8a6a5f1da886353d876bbfba
diegoandres-dt <diegodelatorre155@gmail.com>
2025-07-24 15:25:26 -0500

    diseño de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -42,5 +42,9 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'registro',
+    loadComponent: () => import('./pages/registro/registro.page').then( m => m.RegistroPage)
+  },
 
 ];
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,19 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from "@ionic/angular";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonicModule]
+  imports: [CommonModule, FormsModule, IonicModule,ReactiveFormsModule]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required, Validators.minLength(6)]
+    });
+  }
+
 
   ngOnInit() {
+  }
+
+   onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario válido', this.loginForm.value);
+    } else {
+      console.log('Formulario inválido');
+      this.loginForm.markAllAsTouched(); // Muestra errores al usuario
+    }
   }
 
 }
