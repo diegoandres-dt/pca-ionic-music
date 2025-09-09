fd0f0644296814a61610ffb741764e0dc78a4db1
diegoandres-dt <diegodelatorre155@gmail.com>
2025-09-09 15:23:44 -0500

    mi search terminada

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
-import { personCircle} from 'ionicons/icons';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonIcon, IonSearchbar } from '@ionic/angular/standalone';
+import { personCircle, cameraOutline} from 'ionicons/icons';
+import { IonContent, IonHeader, IonToolbar, IonButtons, IonIcon, IonSearchbar, IonAvatar, IonLabel, IonButton } from '@ionic/angular/standalone';
 import { MiSearchComponent } from "src/app/components/mi-search/mi-search.component";
 
 @Component({
@@ -11,12 +11,12 @@ import { MiSearchComponent } from "src/app/components/mi-search/mi-search.compon
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
   standalone: true,
-  imports: [IonSearchbar, IonIcon, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, MiSearchComponent]
+  imports: [IonButton, IonLabel, IonAvatar, IonSearchbar, IonIcon, IonButtons, IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, MiSearchComponent]
 })
 export class SearchPage implements OnInit {
 
   constructor() { 
-     addIcons({personCircle });
+     addIcons({personCircle, cameraOutline });
   }
 
   ngOnInit() {
